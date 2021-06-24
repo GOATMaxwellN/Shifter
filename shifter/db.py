a5eb05060cafd7b86bb142c638f8d95e7b4f0525
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2021-06-24 15:08:23 -0500

    Added init-db cli command for setting up database functions on app start

--- shifter/db.py
+++ shifter/db.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient
-from flask import g, current_app
 
+
+from flask import g, current_app
+from flask.cli import with_appcontext
+import click
 
 def get_db():
     if "db" not in g:
@@ -16,5 +19,19 @@ def close_db():
         db.close()
 
 
+@click.add_command("init-db")
+def init_db():
+    db = get_db()
+    with db.start_session as session:
+        with session.start_transaction():
+            # drop existing collections
+            db.ShifterDB.drop_collection("users")
+            db.ShifterDB.drop_collection("shifts")
+            # recreate them - technically no, but still yes
+            db.ShifterDB.users
+            db.ShifterDB.shifts
+
+
 def init_app(app):
     app.teardown_appcontext(close_db)
+    app.cli.add_cli_command(init_db)
