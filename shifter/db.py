0843dd83c68145fff2e1fdde2134e364655ef456
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2021-06-25 00:56:26 -0500

    Fixed certificate verification problem

--- shifter/db.py
+++ shifter/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-
+import certifi
 
 from flask import g, current_app
 from flask.cli import with_appcontext
@@ -7,31 +7,31 @@ import click
 
 def get_db():
     if "db" not in g:
-        client = MongoClient(current_app["MONGO_DB_SETTINGS"]["CONNECTION_STRING"])
+        client = MongoClient(
+            current_app.config["MONGO_DB_SETTINGS"]["CONNECTION_STRING"],
+            tlsCAFile=certifi.where(),
+        )
         g.db = client
 
     return g.db
 
 
-def close_db():
+def close_db(e=None):
     db = g.pop("db", None)
     if db is not None:
         db.close()
 
 
-@click.add_command("init-db")
+@click.command("init-db")
+@with_appcontext
 def init_db():
     db = get_db()
-    with db.start_session as session:
-        with session.start_transaction():
-            # drop existing collections
-            db.ShifterDB.drop_collection("users")
-            db.ShifterDB.drop_collection("shifts")
-            # recreate them - technically no, but still yes
-            db.ShifterDB.users
-            db.ShifterDB.shifts
+    
+    # Create 'users' collection
+    coll = db.ShifterDB.users
+    coll.insert_one({"Name": "Maxwell"})
 
 
 def init_app(app):
     app.teardown_appcontext(close_db)
-    app.cli.add_cli_command(init_db)
+    app.cli.add_command(init_db)
