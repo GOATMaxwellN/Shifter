88017e2dc73b7eeef94fd6d503cfe47a0b9956fa
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2021-06-27 23:32:08 -0500

    Mostly finished auth functions
    Created some files to start working on actual calendar editing functions. Come to think of it, I still need to implement OAuth functions to connect to those calendars...

--- shifter/auth.py
+++ shifter/auth.py
@@ -1,11 +1,15 @@
-from bson.codec_options import _parse_codec_options
-from flask import Blueprint, render_template, request
+from flask import (
+    Blueprint, render_template, request, redirect, url_for)
 from hashlib import scrypt
-
 from shifter.db import get_db
+import re
+import os
 
 
 bp = Blueprint("auth", "shifter", url_prefix="/auth")
+
+# Makes sure that passwords have at least a number and uppercase letter
+PASSWORD_VALIDATION_PATTERN = re.compile(r"^(?=.*\d)(?=.*[A-Z])")
 
 
 @bp.route("/login", methods=("GET", "POST"))
@@ -25,7 +29,7 @@ def login():
         else:
             pass_hash = generate_password_hash(password, user["salt"])
             if pass_hash == user["password_hash"]:
-                pass
+                redirect(url_for("calendarview.index"))
             else:
                 error = "Incorrect password"
 
@@ -35,10 +39,47 @@ def login():
 @bp.route("/signup", methods=("GET", "POST"))
 def signup():
     if request.method == "POST":
-        pass
+        username = request["username"]
+        password = request["password"]
 
-    return render_template("auth/signup.html")
+        db = get_db()
+        # Check if username exists already
+        user = None
+        for doc in db.users.find({"username": username}):
+            user = doc
+
+        # If username is not taken, validate the password.
+        if user is None:
+            # If password is valid, add user to database, 
+            # and redirect to login page
+            if validate_password(password):
+                salt = os.urandom(16)
+                pass_hash = generate_password_hash(password, salt)
+                db.users.insert_one({
+                    "username": username,
+                    "password_hash": pass_hash,
+                    "salt": salt,
+                    "connected_calendars": {
+                        "Google": False,
+                        "Outlook": False,
+                    },
+                })
+                redirect(url_for("auth.login"))
+            else:
+                error = "Password must contain at least 6 characters, " \
+                        "1 uppercase letter and a number"
+        else:
+            error = "Username already taken. Pick another one."
+
+    return render_template("auth/signup.html", error=error)
 
 
 def generate_password_hash(password, salt):
     return scrypt(password, salt, n=16384, r=8, p=1)
+
+
+def validate_password(password):
+    if (5 <= len(password) <= 16
+       and PASSWORD_VALIDATION_PATTERN.match(password) is not None):
+       return True
+    return False
