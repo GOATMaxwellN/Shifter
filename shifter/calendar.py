cf0cc950c6cbe6a8b20633fb256d60d5333ad812
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2021-06-30 02:40:16 -0500

    JS makes a calendar!
    HTML page now correctly shows a calendar for the current month, with the ability to switch through them, though there is no button that does it yet.

--- shifter/calendar.py
+++ shifter/calendar.py
@@ -9,7 +9,18 @@ bp = Blueprint("calendarview", "shifter", url_prefix="/calendarview")
 @login_required
 def index():
     user = get_logged_in_user()
+    
+    # Find a calendar that the user has connected.
+    # If the user has connected no calendars, the page
+    # will show links to connect to a calendar
+    calendar = None
+    for cal in user["connected_calendars"]:
+        if user["connected_calendars"][cal]:
+            calendar = cal
+            break
+
     return render_template(
         "calendarview/index.html",
-        username=user["username"]
+        username=user["username"],
+        calendar=calendar,
     )
