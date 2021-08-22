51ca3b1152b5f711a53a2ebccca2824d1b9e26a2
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2021-08-22 01:05:17 -0500

    Add a Privacy Poiicy
    I want to verify my app on the Google Cloud Console, which means I
    have to make a Privacy Policy for my web app. This is pretty rudimentary
    but maybe Google won't be too picky with a simple web app like this.

--- shifter/home.py
+++ shifter/home.py
@@ -3,6 +3,11 @@ from flask import Blueprint, render_template
 bp = Blueprint("home", "shifter")
 
 
-@bp.route("/")
+@bp.get("/")
 def home():
     return render_template("home/index.html")
+
+
+@bp.get("/privacy-policy")
+def privacy_policy():
+    return render_template("privacypolicy/index.html")
