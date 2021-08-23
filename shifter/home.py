ae4b8f661e9b1a6804c3e7be7c4d5e25e8fb9492
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2021-08-23 09:52:10 -0500

    Make Google verification file visible
    Forgot to make an endpoint for the Google verification file.

--- shifter/home.py
+++ shifter/home.py
@@ -11,3 +11,8 @@ def home():
 @bp.get("/privacy-policy")
 def privacy_policy():
     return render_template("privacypolicy/index.html")
+
+
+@bp.get("/google723d18e43c0f1c6c.html")
+def google_verification_file():
+    return render_template("home/google723d18e43c0f1c6c.html")
